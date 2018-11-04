162e86bec67320d7be99aadee73057323a209a5c
vikaskyadav <vikasharry03@gmail.com>
2018-11-04 22:05:25 +0530

    added mocks file

---
+++ src/events_list.js
@@ -0,0 +1,7 @@
+
+    export const events =[
+        {"title":"Pyconf 2017", "subtitle":"Hyderabad","interval":"07-10-2019", "description":"Held at IIIT Hyderabad. Some Talks were really good"},
+        {"title":"Pycon 2018", "subtitle":"Hyderabad","interval":"07-10-2018"},
+        {"title":"GDG 2018", "subtitle":"Hyderabad","interval":"28-10-2018"},
+        {"title":"GDG 2017", "subtitle":"Hyderabad","interval":"04-11-2017"}
+    ];
